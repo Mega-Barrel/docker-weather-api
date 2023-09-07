252b733c1b3718bc05596d06486156b2511caf3f
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-09-07 22:23:00 +0530

    Changed the url to v1 current.json, to get live data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+#TODO
+# Schedule every 30 min cron
+# Dump data into DB
+
 '''App to get weather API data.'''
 import os
 import requests
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def get_api_key():
     """
@@ -26,7 +30,7 @@ def request_data(key, query, dt):
     returns:
         JSON object, else 'Err
     """
-    API_URL = f'http://api.weatherapi.com/v1/history.json?key={key}&q={query}&dt={dt}'
+    API_URL = f'http://api.weatherapi.com/v1/current.json?key={key}&q={query}&dt={dt}'
     resp = requests.get(API_URL)
     
     if resp.status_code == 200:
@@ -41,12 +45,31 @@ def get_weather_data(api_key, query, date):
     """
     resp = request_data(key=api_key, query=query, dt=date)
     city_location = f"{resp['location']['name']}/{resp['location']['country']}"
-    curr_hour = f"{resp['location']['localtime']}"
-    weather = f"Weather: {resp['forecast']['forecastday'][0]['day']['avgtemp_c']}"
-    weather_condition = f"{resp['forecast']['forecastday'][0]['day']['condition']['text']}"
+    last_updated = resp['current']['last_updated']
+    request_time = resp['location']['localtime']
 
-    # print the weather status
-    print(f"{city_location} {curr_hour} {weather} Celsius, {weather_condition}")
+    temp_c = resp['current']['temp_c']
+    temp_f = resp['current']['temp_f']
+    condition = resp['current']['condition']['text']
+    
+    wind_speed_kph = resp['current']['wind_kph']
+    wind_speed_mph = resp['current']['wind_mph']
+    
+    feelslike_c = resp['current']['feelslike_c']
+    feelslike_f = resp['current']['feelslike_f']
+
+    return {
+        'location': city_location,
+        'last_updated': last_updated,
+        'requested_time': request_time,
+        'temp_c': temp_c,
+        'temp_f': temp_f,
+        'weather_condition': condition,
+        'wind_speed_kph': wind_speed_kph,
+        'wind_speed_mph': wind_speed_mph,
+        'feelslike_c': feelslike_c,
+        'feelslike_f': feelslike_f
+    }
 
 def main():
     """
@@ -60,10 +83,11 @@ def main():
     QUERY = 'Mumbai'
 
     # Get previous day's date
-    curr_dt = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+    curr_dt = datetime.now().strftime('%Y-%m-%d')
 
     # Fetch weather data
-    get_weather_data(API_KEY, QUERY, curr_dt)
+    resp = get_weather_data(API_KEY, QUERY, curr_dt)
+    print(resp)
 
 if __name__ == '__main__':
     main()
